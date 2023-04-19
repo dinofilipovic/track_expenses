d142f8903e5f8686380e11096df6bda342b9bb1c
Dino FIlipović <filipovic.dino@gmail.com>
2023-04-19 12:31:01 +0200

    Create chart.dart
    Adding Chart Widget and planning further development.

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({super.key});
+  List <Map<String, Object>> get groupedTransactionValues 
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
