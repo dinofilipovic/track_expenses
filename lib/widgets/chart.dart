fd04e41a60e3087116fd5c5471d6094231c8321b
Dino FIlipović <filipovic.dino@gmail.com>
2023-04-24 19:12:19 +0200

    Chart Widget
    - Added Chart custom Widget
    - Modified main.dart & chart.dart added constructor and created logic to populate list.

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,12 +1,29 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+// Custom Widgets
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({super.key});
-  List <Map<String, Object>> get groupedTransactionValues {
-    return List.generate(7, (index) => null)
+  final List<Transaction> recentTransaction;
+
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      var totalSum = 0.0;
+      for (var i = 0; i < recentTransaction.length; i++) {
+        if (recentTransaction[i].date.day == weekDay.day &&
+            recentTransaction[i].date.month == weekDay.month &&
+            recentTransaction[i].date.year == weekDay.year) {
+          totalSum += recentTransaction[i].amount;
+        }
+      }
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
   }
 
   @override
